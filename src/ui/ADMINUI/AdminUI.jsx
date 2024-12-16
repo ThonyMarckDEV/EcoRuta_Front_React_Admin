db733ffe5f444a2c2687e6b925b14f7bb611b744
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-16 15:32:04 -0500

    aaaa

--- src/ui/ADMINUI/AdminUI.jsx
+++ src/ui/ADMINUI/AdminUI.jsx
@@ -1,12 +1,32 @@
 import React, { useState } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from './firebase'; // Asegúrate de importar tu instancia de auth de Firebase
 
-function Login()  {
- 
+function AdminUI() {
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);  // Desloguea al usuario
+      console.log("Usuario deslogueado");
+      // Aquí puedes redirigir a la página de inicio de sesión o realizar alguna acción
+      window.location.href = '/'; // Redirige a la página de inicio
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error.message);
+    }
+  };
+
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50">
-    
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h2 className="text-xl font-bold mb-4">¡Bienvenido!</h2>
+        <button
+          onClick={handleLogout}
+          className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-500 focus:outline-none"
+        >
+          Cerrar sesión
+        </button>
+      </div>
     </div>
   );
-};
+}
 
-export default Login;
+export default AdminUI;
